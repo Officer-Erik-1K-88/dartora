fcf9d9548d630f0d3733a1a436124ec8a99777d1
Officer-Erik-1K-88 <oficer.erik.k@gmail.com>
2025-08-27 14:37:37 -0500

    Added more tests

--- test/math/matrix_test.dart
+++ test/math/matrix_test.dart
@@ -10,8 +10,7 @@ void main() {
     ]);
 
     test('rowEchelon', () {
-      final rowEchelon = matrix.rowEchelon;
-      expect(rowEchelon, wrapMatcher(Matrix([
+      expect(matrix.rowEchelon, wrapMatcher(Matrix([
         [1, 2, 3],
         [0, -3, -6],
         [0, 0, 0],
@@ -19,14 +18,27 @@ void main() {
     });
 
     test('reducedRowEchelon', () {
-      final reducedRowEchelon = matrix.reducedRowEchelon;
-      expect(reducedRowEchelon, wrapMatcher(Matrix([
+      expect(matrix.reducedRowEchelon, wrapMatcher(Matrix([
         [1, 0, -1],
         [0, 1, 2],
         [0, 0, 0],
       ])));
     });
 
-    
+    test('identity', () {
+      expect(matrix.identity, wrapMatcher(Matrix([
+        [1, 0, 0],
+        [0, 1, 0],
+        [0, 0, 1],
+      ])));
+    });
+
+    test('rank', () {
+      expect(matrix.rank, equals(2));
+    });
+
+    test('determinant', () {
+      expect(matrix.determinant, equals(0));
+    });
   });
 }
